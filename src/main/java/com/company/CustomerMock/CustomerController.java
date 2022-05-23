ce9acbbcb294a67aacc4a6c14066c181c917225f
guru-23 <gururaj.cts@gmail.com>
2022-05-23 06:11:44 +0100

    test branch change

--- src/main/java/com/company/CustomerMock/CustomerController.java
+++ src/main/java/com/company/CustomerMock/CustomerController.java
@@ -14,12 +14,12 @@ public class CustomerController {
     @RequestMapping(path = "/customer", method = RequestMethod.POST,
             consumes = { MediaType.APPLICATION_XML_VALUE },
             produces = { MediaType.APPLICATION_XML_VALUE })
-    public Customer createCustomer(@RequestBody Customer customer){
+    public Customer hello(@RequestBody Customer customer){
         System.out.println("Input request>>"+customer);
         System.out.println("test branch change");
-        String customerId = String.valueOf((int) (Math.random() * 100000));
-        customer.setId(customerId);
-        customers.put(customerId, customer);
+        String id = "99999";
+        customer.setId(id);
+        customers.put(id, customer);
         return customer;
     }
 
